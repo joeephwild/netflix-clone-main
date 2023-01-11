4c9c0c2c0f0766a9cf36ecf49e879068b56cd833
Joseph Omotade <josephomotade0@gmail.com>
2023-01-11 11:11:46 -0800

    commit

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -31,7 +31,7 @@ function Movies({ movie, large }) {
   return (
     <div className="pl-6 relative w-[250px] ">
       <>
-        <img className={`max-w-[120px] h-[200px] block object-cover rounded-br-lg rounded-tl-lg ${large && "h-[400px] max-w-[190px]"}`} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+        <img className={`max-w-[120px] h-[200px] block object-cover rounded-br-lg rounded-tl-lg ${large && "h-[400px] max-w-[200px]"}`} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
       </>
       <div onClick={saveShow} className="absolute bg-red-600 rounded-br-lg top-0 p-2 right-o text-white">
         {like ? <AiFillHeart size={15} /> : <FaRegHeart size={15} />}
